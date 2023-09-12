badfdbeaf0d9b10056ea43c45704049870cebc7e
quantsr <48443553+quantsr@users.noreply.github.com>
2023-09-11 21:42:31 -0300

    Update Equipamentos.java inner classes

--- src/library/Equipamentos.java
+++ src/library/Equipamentos.java
@@ -2,17 +2,35 @@ package library;
 
 public class Equipamentos {
     class Informatica{
-        
+        String modelo;
+        String codigoModelo;
+        enum TipoDeRecurso{
+            COMPUTADOR,
+            TABLET,
+            PROJETOR
+        }
     }
     class Audiovisual{
-
+        String modelo;
+        String codigoModelo;
+        enum TipoDeRecurso{
+            CAMERA,
+            MICROFONE,
+            CAIXA_DE_SOM
+        }
     }
-    class Impressão{
-
+    class Impressao{
+        String modelo;
+        String codigoModelo;
+        enum TipoDeRecurso{
+            IMPRESSORA,
+            SCANNER,
+            COPIADORA
+        }
     }
     enum CategoriaEquipamento{
         INFORMATICA,
         AUDIOVISUAL,
-        IMPRESSÃO
+        IMPRESSAO
     }
 }
