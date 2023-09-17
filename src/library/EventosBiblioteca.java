dbf66d1f78adf8f0dc632078d554d68d6bbb5ec0
JoaoPedroMCampos <142697433+JoaoPedroMCampos@users.noreply.github.com>
2023-09-17 13:33:09 -0300

    Created EventosBiblioteca Collection

--- src/library/EventosBiblioteca.java
+++ src/library/EventosBiblioteca.java
@@ -1,8 +1,36 @@
 package library;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class EventosBiblioteca {
+    
+    public static void membrosEVentos (String[] args) {
+        
+        List<List<String>> eventos; //cria a lista de eventos
+        //adiciona um novo evento
+        eventoUm = new ArrayList<List<String>>();
+        //adiciona uma pessoa ao eventoUm
+        eventoUm.get(0).add(new String("João Melo"));
+        eventoUm.get(1).add(new String("Max Arruda"));
+        eventoUm.get(2).add(new String("Miguel Vargas"));
+
+        // Acessando membros por posições
+        String eventos = eventos.get(1);
+        for (String i = 0; i < eventos.size(); i++){
+            eventos.get(i, membros.get(i));
+        } 
+
+        // Iterando pela lista
+        for (String evento : eventos) {
+            for (String membro : membros) {
+                System.out.println(evento, membro);
+            }
+        }
+    }
+
     class Palestra{
         String palestrante;
         String topico;
