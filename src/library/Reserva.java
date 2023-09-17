acdddad6daf82a2743adfa6d2b0a245cc8984ee1
JoaoPedroMCampos <142697433+JoaoPedroMCampos@users.noreply.github.com>
2023-09-17 13:28:58 -0300

    Created Reservas Collection

--- src/library/Reserva.java
+++ src/library/Reserva.java
@@ -1,5 +1,7 @@
 package library;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.time.LocalDate;
 
 public class Reserva {
@@ -7,6 +9,24 @@ public class Reserva {
     private Membro donoReserva; //associação
     private LocalDate diaRetirada; //composição
 
+    public static void listaReservas (String[] args) {
+        List<String> reservas = new LinkedList<>();
+
+        // Adicionando reservas
+        reservas.add("O Hobbit, João Pedro Melo, 20/09");
+        reservas.add("Uma breve historia do tempo, Max Arruda, 21/09");
+        reservas.add("Introdução a algoritmos, Miguel Vargas, 22/09");
+
+        // Acessando reservas por posições
+        String reservas = reservas.get(1); //"O Hobbit"
+        String reservas = reservas.get(2); //"Uma breve historia do tempo"
+        String reservas = reservas.get(3); //"Introdução a algoritimos"
+
+        //Iterando pela lista
+        for (String reserva : reservas) {
+            System.out.println(reserva);
+        }
+    }   
     
     public Reserva(ItemMultimidia item, Membro donoReserva, LocalDate diaRetirada) {
         this.item = item;
